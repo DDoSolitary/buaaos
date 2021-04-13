a86ddef1599a55227b38a3b44277c8e04345a104
DDoSolitary <DDoSolitary@gmail.com>
2021-04-13 21:00:57 +0800

    lab3: implement task 3

--- lib/env.c
+++ lib/env.c
@@ -60,8 +60,12 @@ int envid2env(u_int envid, struct Env **penv, int checkperm)
     struct Env *e;
     /* Hint: If envid is zero, return curenv.*/
     /*Step 1: Assign value to e using envid. */
-
-
+    if (envid == 0) {
+        *penv = curenv;
+        return 0;
+    } else {
+        e = &envs[ENVX(envid)];
+    }
 
     if (e->env_status == ENV_FREE || e->env_id != envid) {
         *penv = 0;
@@ -75,9 +79,10 @@ int envid2env(u_int envid, struct Env **penv, int checkperm)
      *     or an immediate child of curenv.
      *     If not, error! */
     /*     Step 2: Make a check according to checkperm. */
-
-
-
+    if (checkperm && e != curenv && e->env_parent_id != curenv->env_id) {
+        *penv = 0;
+        return -E_BAD_ENV;
+    }
 
     *penv = e;
     return 0;
