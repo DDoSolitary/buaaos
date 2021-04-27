7cc4d8d8a927bfe472e56ea3a49637bb46513b7d
DDoSolitary <DDoSolitary@gmail.com>
2021-04-27 14:19:55 +0800

    lab3-2-exam: implement part 1

--- lib/sched.c
+++ lib/sched.c
@@ -2,6 +2,10 @@
 #include <pmap.h>
 #include <printf.h>
 
+
+#define PRI(pri) ((pri) & 0xff)
+#define PRI_FUNC1(pri) (((pri) >> 8) & 0xff)
+
 /* Overview:
  *  Implement simple round-robin scheduling.
  *
@@ -14,38 +18,16 @@
 /*** exercise 3.14 ***/
 void sched_yield(void)
 {
-    static int count = 0; // remaining time slices of current env
-    static int point = 0; // current env_sched_list index
-    struct Env *e;
-    
-    /*  hint:
-     *  1. if (count==0), insert `e` into `env_sched_list[1-point]`
-     *     using LIST_REMOVE and LIST_INSERT_TAIL.
-     *  2. if (env_sched_list[point] is empty), point = 1 - point;
-     *     then search through `env_sched_list[point]` for a runnable env `e`, 
-     *     and set count = e->env_pri
-     *  3. count--
-     *  4. env_run()
-     *
-     *  functions or macros below may be used (not all):
-     *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
-     */
-    if (curenv != NULL) {
-        if (count == 0) {
-            LIST_REMOVE(curenv, env_sched_link);
-            LIST_INSERT_TAIL(&env_sched_list[1 - point], curenv, env_sched_link);
-        } else {
-            count--;
-            env_run(curenv);
+    struct Env *e = NULL, *tmp_e;
+
+    LIST_FOREACH(tmp_e, &env_sched_list[0], env_sched_link) {
+        if (tmp_e->env_status == ENV_RUNNABLE) {
+            if (e == NULL || PRI(tmp_e->env_pri) > PRI(e->env_pri)) {
+                e = tmp_e;
+            }
         }
     }
-    if (LIST_EMPTY(&env_sched_list[point])) {
-        point = 1 - point;
-    }
-    LIST_FOREACH(e, &env_sched_list[point], env_sched_link) {
-        if (e->env_status == ENV_RUNNABLE) {
-            count = e->env_pri - 1;
-            env_run(e);
-        }
+    if (e != NULL) {
+        env_run(e);
     }
 }
